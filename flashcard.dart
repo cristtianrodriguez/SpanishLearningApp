454d283e9f4db545d043493cb400d7214d9ee3c9
cristtianrodriguez <cristianrodriguez7992@gmail.com>
2025-04-15 14:37:48 -0400

    Update and rename model.dart to flashcard.dart

---
+++ flashcard.dart
@@ -0,0 +1,13 @@
+class Flashcard {
+  String spanish;
+  String english;
+  List<String> options;
+  String correctOption;
+
+  Flashcard({
+    required this.spanish,
+    required this.english,
+    required this.options,
+    required this.correctOption,
+  });
+}
